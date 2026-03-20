eb424bc240a3c6888094f8520b6a976d444e3a23
ivan <ivansevryukov358@gmail.com>
2026-03-20 08:48:27 +0100

    add ScheduleControllers

---
+++ ScheduleSIA/Controllers/ScheduleController.cs
@@ -0,0 +1,25 @@
+﻿using ScheduleSIA.Data;
+using ScheduleSIA.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ScheduleSIA.Controllers
+{
+    [ApiController]
+    [Route("api/schedule")]
+    public class ScheduleController : ControllerBase
+    {
+        private readonly IScheduleService _service;
+        public ScheduleController(IScheduleService service, AppDbContext db)
+        {
+            _service = service;
+        }
+
+        [HttpGet("group/{grounName}")]
+
+        public async Task<IActionResult> GetSchedule(string grounName, DateTime start, DateTime end)
+        {
+            var result = await _service.GetScheduleForGroup(grounName, start.Date, end.Date);
+            return Ok(result);
+        }
+    }
+}
