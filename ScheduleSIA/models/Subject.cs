65e2e6cea8d594d505642a9b659b0108b3a5cc48
ivan <ivansevryukov358@gmail.com>
2026-03-20 08:37:31 +0100

    final models

---
+++ ScheduleSIA/models/Subject.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ScheduleSIA.models
+{
+    [Table("subject")]
+    public class Subject
+    {
+        [Key]
+        [Column("subject_id")]
+        public int SubjectId { get; set; }
+
+        [Column("name")]
+        [Required]
+        public string Name { get; set; } = null!;
+    }
+}
---
+++ ScheduleSIA/models/Weekday.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ScheduleSIA.models
+{
+    [Table("weekday")]
+    public class Weekday
+    {
+        [Key]
+        [Column("weekday_id")]
+        public int WeekdayId { get; set; }
+
+        [Column("name")]
+        [Required]
+        public string Name { get; set; } = null!;
+    }
+}
