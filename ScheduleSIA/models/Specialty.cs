66373c0b96d2bf92f939eb6d717e88b50b0e9754
ivan <ivansevryukov358@gmail.com>
2026-03-20 08:36:06 +0100

    add models four models

---
+++ ScheduleSIA/models/Specialty.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ScheduleSIA.models
+{
+    [Table("specialties")]
+    public class Specialty
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("name")]
+        [Required]
+        public string Name { get; set; } = null!;
+    }
+}
